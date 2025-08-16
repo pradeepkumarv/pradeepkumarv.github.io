d6eb1a6b54f21d860047c5c288c50d94faa1fe94
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-08-16 21:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // supabaseClient.js
-const SUPABASE_URL = "https://dobiarrtabqiozfitsoz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvYmlhcnJ0YWJxaW96Zml0c296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNjk0MTcsImV4cCI6MjA3MDg0NTQxN30.JTIdjABW7pceVfBWETKA9tnGFaBxQW4mf3VFHtWe3Fw";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-export { supabaseClient };
+export const supabase = createClient(
+  "https://dobiarrtabqiozfitsoz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvYmlhcnJ0YWJxaW96Zml0c296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNjk0MTcsImV4cCI6MjA3MDg0NTQxN30.JTIdjABW7pceVfBWETKA9tnGFaBxQW4mf3VFHtWe3Fw"
+);
+
+
+
+
