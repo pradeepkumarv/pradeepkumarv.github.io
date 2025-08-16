7c8ec2fbc17145b8e7863dae3eaaa803367c0510
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-08-16 22:10:49 +0530

    Create supabaseclient.js

---
+++ supabaseclient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://dobiarrtabqiozfitsoz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvYmlhcnJ0YWJxaW96Zml0c296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNjk0MTcsImV4cCI6MjA3MDg0NTQxN30.JTIdjABW7pceVfBWETKA9tnGFaBxQW4mf3VFHtWe3Fw"
+);
