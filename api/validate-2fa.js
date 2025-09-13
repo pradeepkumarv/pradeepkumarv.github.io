424f9706a9a97bcc66c2fd9e1df334b65e117903
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-09-13 22:11:52 +0530

    Update validate-2fa.js

--- api/validate-2fa.js
+++ api/validate-2fa.js
@@ -1,7 +1,7 @@
 import { serialize } from 'cookie';
 
 function setCors(res) {
-  const FRONTEND = process.env.FRONTEND_URL || 'https://pradeepkumarv-github-g4z3mkshe-pradeep-kumar-vs-projects.vercel.app';
+  const FRONTEND = process.env.FRONTEND_URL || 'https://pradeepkumarv.github.io';
   res.setHeader('Access-Control-Allow-Origin', FRONTEND);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
