326096eafe034f062ab40d1c84157600201ba8b4
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-09-13 22:08:23 +0530

    Update request-otp.js

--- api/request-otp.js
+++ api/request-otp.js
@@ -1,9 +1,10 @@
 function setCors(res) {
-  const FRONTEND = process.env.FRONTEND_URL || 'https://pradeepkumarv-github-g4z3mkshe-pradeep-kumar-vs-projects.vercel.app';
+  const FRONTEND = process.env.FRONTEND_URL || 'https://pradeepkumarv.github.io';
   res.setHeader('Access-Control-Allow-Origin', FRONTEND);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
+
 
 export default async function handler(req, res) {
   setCors(res);
