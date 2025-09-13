4c5b2bc4e238ae07f5641622155764e9e2208c0c
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-09-13 22:45:32 +0530

    Update holdings.js

--- api/holdings.js
+++ api/holdings.js
@@ -1,10 +1,19 @@
 import cookie from 'cookie';
 
 function setCors(res) {
-  const FRONTEND = process.env.FRONTEND_URL || 'https://pradeepkumarv.github.io';
-  res.setHeader('Access-Control-Allow-Origin', FRONTEND);
+  const allowedOrigins = [
+    https://pradeepkumarv.github.io',          // your frontend (GitHub Pages)
+    'https://pradeepkumarv-github-io.vercel.app' // your backend domain
+  ]
+   const origin = res.req?.headers?.origin || ''; // fallback if available
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
 
 export default async function handler(req, res) {
