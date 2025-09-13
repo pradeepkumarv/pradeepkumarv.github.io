05242994783a8d3ca0355f78bdc1369871e2ea3c
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-09-13 23:40:46 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -17,7 +17,7 @@ function setCors(res) {
 export default async function handler(req, res) {
   setCors(res);
   if (req.method === 'OPTIONS') return res.status(204).end();
-  if (req.method !== 'POST') return res.status(405).json({ error: 'method not allowed' });
+ 
 
   try {
     const { username, mobile } = req.body;
